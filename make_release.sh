748aca7aa0ef21f523cfbbc4397c6b630f86aded
jiangwei <jiangwei@netxeon.com>
2018-07-27 12:55:11 +0800

    add recommend list

--- make_release.sh
+++ make_release.sh
@@ -2,6 +2,7 @@
 
 CUR_DIR=`pwd`
 python xk_appsTxt_generator.py 
+python xk_appsRcmdTxt_generator.py
 if [ $? -ne 0 ]
 then
         echo -e "\033[31m  fail to generate app_xml!!! \033[0m"
@@ -9,6 +10,8 @@ then
 else
 		mv appMap.xml apps.txt
         echo -e "\033[32m apps.txt generate successfully \033[0m"
+                mv appMap_recommond.xml commend.txt
+        echo -e "\033[32m commend.txt generate successfully \033[0m"
 fi
 git add -A
 #git commit -m 'release 1.1.0'
---
+++ xk_appsRcmdTxt_generator.py
@@ -0,0 +1,94 @@
+# coding=UTF-8
+# Script to generate the xml files for app
+
+import os
+import sys
+import md5
+
+def main():
+  reload(sys)
+  sys.setdefaultencoding('utf8')
+  topdir = os.getcwd()
+
+  # app list
+  os.chdir(topdir)
+  dirs = (os.listdir('.'))
+  #recommend applist
+  recommend = open("recommend.txt")
+  recommend_list = recommend.readline()
+  #print recommend_list
+  # final apps text
+  appMap_xml = u"["
+  # loop thru and add each apps addon.xml file
+  for app_dir in dirs:
+    try:
+      os.chdir(topdir)
+      if(not os.path.isdir(app_dir)):
+        continue
+      if(app_dir.startswith('.')):
+        # skip hidden dirs
+        continue
+      os.chdir(topdir + "/" + app_dir)
+      #print app_dir
+      if(recommend_list.find(app_dir) == -1):
+	#print "no recommend"
+        continue
+      #print "recommended"
+      files = (os.listdir('.'))
+      filesTmp = []
+      for fileTmp in files:
+          if(fileTmp.startswith("appname.txt")):
+	      filesTmp.insert(0 , fileTmp)
+          elif(fileTmp.endswith(".apk") or fileTmp.endswith(".mp4") or fileTmp.endswith(".MOV")):
+              filesTmp.append(fileTmp)
+
+      for file in filesTmp:
+          if(file.startswith("appname.txt")):
+              f = open(file)
+              aPP_NAME_extra = f.readline()
+	      #print aPP_NAME_extra
+              f.close()
+              continue
+          if(not file.startswith(app_dir + '-')):
+              continue
+	  #print file
+          app_name = file[0:file.rfind("-")]
+	  if(file.endswith(".apk")):
+              app_version = file[file.rfind("-")+1:file.rfind(".apk")]
+          elif(file.endswith(".mp4")):
+              app_version = file[file.rfind("-")+1:file.rfind(".mp4")]
+          elif(file.endswith(".MOV")):
+              app_version = file[file.rfind("-")+1:file.rfind(".MOV")]
+          app_md5 = md5.new(open(file).read()).hexdigest()
+          #print app_md5
+          app_xml = ""
+          app_xml += "{\"aPP_ID\":\"com.trigtop." + app_name + "\","
+          app_xml += "\"aPP_NAME\":\"" + app_name + "\","
+	  app_xml += "\"aPP_NAME_extra\":\"" + aPP_NAME_extra + "\","
+          app_xml += "\"iMAGE_PATH\":\"https://raw.githubusercontent.com/trigtop/apps/Ergo/" + app_name + "/" + app_name + ".png\","
+          app_xml += "\"mD5\":\"" + app_md5 + "\","
+          app_xml += "\"uRL_PATH\":\"https://raw.githubusercontent.com/trigtop/apps/Ergo/" + app_name + "/" + file + "\","
+          app_xml += "\"vER\":\"" + app_version + "\""
+          appMap_xml += app_xml.rstrip() + u"},\n"
+    except Exception, e:
+      # missing or poorly formatted app.xml
+      print e
+      print "Failed to add xml"
+      sys.exit(1)
+  # clean and add closing tag
+  appMap_xml = appMap_xml.strip() + u"]\n"
+  # save file
+  try:
+    # write data to the file
+    os.chdir(topdir)
+    open( "appMap_recommond.xml", "w" ).write( appMap_xml.encode( "UTF-8" ) )
+    print "appMap_recommond.xml generated!!!"
+    sys.exit(0)
+  except Exception, e:
+    # oops
+    print "An error occurred saving %s file!\n%s" % ( "appMap_recommond.xml", e, )
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+  main()
